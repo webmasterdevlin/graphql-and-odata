dbbc396585747be558c24e48563429ad66848f49
Devlin D <webmasterdevlin>
2020-08-07 08:30:25 +0200

    updated

---
+++ TheGameShop.Mobile/TheGameShop.Mobile/Constants/BackendConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TheGameShop.Mobile.Constants
+{
+    public static class BackendConstants
+    {
+#if DEBUG
+        public static string GraphQLApiUrl { get; } = Device.RuntimePlatform is Device.Android ? "http://10.0.2.2:5001/" : "http://localhost:5001/";
+#else
+#error Missing GraphQL Api Url
+        public static string GraphQLApiUrl { get; } = "";
+#endif
+    }
+}
---
+++ TheGameShop.Mobile/TheGameShop.Mobile/Services/GraphQLService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using Polly;
+using TheGameShop.Mobile.Constants;
+using TheGameShop.Mobile.Models;
+
+namespace TheGameShop.Mobile.Services
+{
+    public static class GraphQLService
+    {
+        private static readonly Lazy<GraphQLHttpClient> _clientHolder = new Lazy<GraphQLHttpClient>(CreateGraphQLClient);
+
+        private static GraphQLHttpClient Client => _clientHolder.Value;
+
+        public static async Task<List<Game>> GetGames()
+        {
+            var graphQLRequest = new GraphQLRequest
+            {
+                Query = "query { games  { id, name } }"
+            };
+
+            var data = await AttemptAndRetry(() => Client
+                .SendQueryAsync<GameGraphQLResponse>(graphQLRequest))
+                .ConfigureAwait(false);
+
+            return data.Games;
+        }
+
+        private static GraphQLHttpClient CreateGraphQLClient()
+        {
+            var options = new GraphQLHttpClientOptions
+            {
+                EndPoint = new Uri(BackendConstants.GraphQLApiUrl),
+#if !DEBUG
+                HttpMessageHandler = new ModernHttpClient.NativeMessageHandler()
+#endif
+            };
+
+            return new GraphQLHttpClient(options, new NewtonsoftJsonSerializer());
+        }
+
+        private static async Task<T> AttemptAndRetry<T>(Func<Task<GraphQLResponse<T>>> action, int numRetries = 2)
+        {
+            var response = await Policy.Handle<Exception>().WaitAndRetryAsync(numRetries, pollyRetryAttempt).ExecuteAsync(action).ConfigureAwait(false);
+
+            if (response.Errors != null && response.Errors.Count() > 1)
+                throw new AggregateException(response.Errors.Select(x => new Exception(x.ToString())));
+            else if (response.Errors != null && response.Errors.Any())
+                throw new Exception(response.Errors.First().ToString());
+
+            return response.Data;
+
+            static TimeSpan pollyRetryAttempt(int attemptNumber) => TimeSpan.FromSeconds(Math.Pow(2, attemptNumber));
+        }
+    }
+}
